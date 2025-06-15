695d5736d954d13d83dd4dc7a69714e983823470
tungtq123 <he182488toquoctung@gmail.com>
2025-06-15 12:24:05 +0700

    upload folder front-end

---
+++ src/app/confirm-checkout/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+import React from "react";
+import CheckoutPage from "@/components/Home/Cart/CheckoutPage";
+
+export default function Checkout() {
+    return <CheckoutPage />;
+}
---
+++ src/app/order-history/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+import React from "react";
+import OrderHistory from "@/components/Home/Order/OrderHistory";
+
+export default function CartPage() {
+    return <OrderHistory />;
+}
