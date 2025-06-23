52580a7a9cebf6183984e601f7a9cb2c7c5c55fd
Tên của bạn <q314e25r@gmail.com>
2025-06-23 23:55:42 +0700

    Add test

--- frontend/src/pages/dashboard/index.js
+++
@@ -1,6 +0,0 @@
-export * from "@/pages/dashboard/home";
-export * from "@/pages/dashboard/profile";
-export * from "@/pages/dashboard/users.jsx";
-export * from "@/pages/dashboard/notifications";
-export * from "@/pages/dashboard/posts.jsx"
-export * from "@/pages/dashboard/postTopics.jsx"
